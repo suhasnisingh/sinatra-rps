d902911f21ed8cc7c6d1c5ee27bb266aed530911
suhasnisingh <165236685+suhasnisingh@users.noreply.github.com>
2024-04-24 19:55:23 +0000

    complete

--- app.rb
+++ app.rb
@@ -1,9 +1,50 @@
 require "sinatra"
 require "sinatra/reloader"
 
+rps_array = ["rock", "paper", "scissors"]
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+  @comp_play = rps_array.sample
+
+  if @comp_play == "rock"
+    @outcome = "We tied!"
+  elsif @comp_play == "paper"
+    @outcome = "We lost!" 
+  else @comp_play == "scissors"
+    @outcome = "We won!"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  @comp_play = rps_array.sample
+
+  if @comp_play == "rock"
+    @outcome = "We won!"
+  elsif @comp_play == "paper"
+    @outcome = "We tied!" 
+  else @comp_play == "scissors"
+    @outcome = "We lost!"
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  @comp_play = rps_array.sample
+
+  if @comp_play == "rock"
+    @outcome = "We lost!"
+  elsif @comp_play == "paper"
+    @outcome = "We won!" 
+  else @comp_play == "scissors"
+    @outcome = "We tied!"
+  end
+
+  erb(:scissors)
 end
